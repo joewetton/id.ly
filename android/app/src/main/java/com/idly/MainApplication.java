ace59b1544ae9991fa715e193a5d2d83a17f185b
ad25 <ad23@pdx.edu>
2018-05-14 17:16:42 -0700

    installed additional libaries

--- android/app/src/main/java/com/idly/MainApplication.java
+++ android/app/src/main/java/com/idly/MainApplication.java
@@ -3,9 +3,11 @@ package com.idly;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.reactnative.camera.RNCameraPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.rctunderdark.NetworkManagerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNCameraPackage(),
             new ImagePickerPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new NetworkManagerPackage()
       );
     }
 
